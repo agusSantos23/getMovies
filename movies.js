07b7d4308a80ccfdade04bd6ff329fd6483820b9
Agustin <agustindelossantos023@gmail.com>
2025-04-07 21:12:53 +0200

    FIX errors data

--- movies.js
+++ movies.js
@@ -1,132 +1,135 @@
 import express from 'express';
-import { fetchData, saveMovieToDatabase, saveGenreToDatabase, saveMovieGenreRelation} from './utils/apiMovies.js';
+import { fetchData, saveMovieToDatabase, saveGenreToDatabase, saveMovieGenreRelation, processAndUploadImage } from './utils/apiMovies.js';
 import pool from './utils/db.js';
 
 process.loadEnvFile();
 
 const router = express.Router();
 
-
-
-const processGenres = async (genresMovie, dbMovieId) => {
-  const connection = await pool.getConnection(); 
+const processGenres = async (genresMovie, dbMovieId, connection) => {
 
   if (genresMovie && genresMovie.length > 0) {
 
     console.log('Genres of the film:', genresMovie);
+    console.log('Genres of the film:', dbMovieId);
 
     for (const genre of genresMovie) {
+
       try {
         let dbGenreId;
-
         console.log('Processing gender:', genre.name);
-
-        const [existingGenre] = await connection.execute('SELECT id FROM genres WHERE name = ?',[genre.name]);
+        const [existingGenre] = await connection.execute('SELECT id FROM genres WHERE name = ?', [genre.name]);
 
         if (existingGenre.length > 0) {
-          //Get Id
           dbGenreId = existingGenre[0].id;
           console.log(`Gender "${genre.name}" already exists with ID:`, dbGenreId);
-
         } else {
-          //Save and Get Id
           dbGenreId = await saveGenreToDatabase(genre.name, connection);
           console.log(`Gender "${genre.name}" saved with ID:`, dbGenreId);
 
+          
         }
 
         console.log(`Relationship: ID movie: ${dbMovieId}, Gender ID: ${dbGenreId}`);
-
-        await saveMovieGenreRelation(dbMovieId, dbGenreId);
-
+        await saveMovieGenreRelation(dbMovieId, dbGenreId, connection);
         console.log('Saved relationship.');
+        
       } catch (error) {
         console.error('Error al procesar el género:', error);
         throw error;
       }
     }
+
   }
 };
 
 const getNext20Movies = async (startMovieId) => {
-  const connection = await pool.getConnection(); 
-  const movies = [];
+  const connection = await pool.getConnection();
   let currentMovieId = startMovieId;
   let fetchedCount = 0;
 
   try {
-    await connection.beginTransaction(); 
+    await connection.beginTransaction();
 
-    while (movies.length < 20) {
+    while (fetchedCount < 20) {
+      console.log("fetchedCount:", fetchedCount);
+
       try {
-        const movieDetails = fetchData(currentMovieId);
 
-        if ( !movieDetails || movieDetails.adult || (movieDetails.title === undefined && !movieDetails.success) ) {
+        let movieDetails = await fetchData(currentMovieId);
+
+
+        if (!movieDetails || movieDetails.adult || (movieDetails.title === undefined && !movieDetails.success)) {
           currentMovieId++;
           continue;
         }
 
         console.log("Film:", movieDetails.title);
-
-        const [existingMovie] = await connection.execute('SELECT id FROM movies WHERE title = ?',[movieDetails.title]);
+        const [existingMovie] = await connection.execute('SELECT id FROM movies WHERE title = ?', [movieDetails.title]);
 
         if (existingMovie.length > 0) {
-
           console.log(`The movie ${movieDetails.title} already exists.`);
           currentMovieId++;
           continue;
-
         } else {
           console.log('Keeping movie:', movieDetails.title);
-          const dbMovieId = await saveMovieToDatabase(movieDetails); 
-
-          await processGenres(movieDetails.genre, dbMovieId);
-
-          console.log(`Genres processed for the film: ${movieDetails.title}`);
-
-          movies.push(movieDetails);
+          const dbMovieId = await saveMovieToDatabase(movieDetails, connection);
+          
+          if (dbMovieId) {
+            await processGenres(movieDetails.genres, dbMovieId, connection);
+            console.log(`Genres processed for the film: ${movieDetails.title}`);
+          }
+          
           fetchedCount++;
-          console.log(`Movie and generos aggregates: ${movieDetails.title}`);
+
+          console.log("next");
+          
         }
       } catch (error) {
-        console.error(`Error al obtener o guardar la película con ID ${currentMovieId}:`,error);
-        throw error; 
-
+        console.error(`Error processing movie with ID ${currentMovieId}:`, error);
+        throw error;
       } finally {
         currentMovieId++;
       }
     }
 
     await connection.commit();
-    console.log('Completed transaction.');
+    console.log('Completed transaction for 20 movies.');
 
   } catch (error) {
-    await connection.rollback(); 
-    console.error('Reversal transaction due to an error:', error);
+    await connection.rollback();
+    console.error('Transaction rollback due to an error:', error);
   } finally {
-    connection.release(); 
+    connection.release();
   }
 
-  return movies;
+  return;
 };
 
 router.post('/getM', async (req, res) => {
   try {
     const movieIds = req.body.movieIds;
+    const delayBetweenBatches = 5000;
 
     if (!movieIds || !Array.isArray(movieIds)) {
-      return res.status(400).json({ error: 'An array of films IDS is required.' });
+      return res.status(400).json({ error: 'An array of film IDs is required.' });
     }
+
+    console.log(`Processing movie IDs: ${movieIds}`);
 
     for (const startMovieId of movieIds) {
       await getNext20Movies(startMovieId);
-    }
 
-    res.json({ message: 'Films and genres obtained and saved correctly.' });
+      console.log(`Waiting ${delayBetweenBatches / 1000} seconds before processing the next batch.`);
+      await new Promise((resolve) => setTimeout(resolve, delayBetweenBatches));
+    }
+    
+
+    res.json({ message: 'Films and genres obtained and saved correctly in batches.' });
 
   } catch (error) {
     console.error('Route /getm error:', error);
-    res.status(500).json({ error: 'Error obtaining and saving movies and genres.' });
+    res.status(500).json({ error: 'Error obtaining and saving movies and genres in batches.' });
   }
 });
 
--- utils/apiMovies.js
+++ utils/apiMovies.js
@@ -2,15 +2,16 @@ import fetch from 'node-fetch';
 import sharp from 'sharp';
 import { v4 as uuidv4 } from 'uuid';
 
+process.loadEnvFile();
+
+
 const tmdbApiKey = process.env.TMDB_API_KEY;
 const imgbbApiKey = process.env.IMG_BB_API_KEY;
 
 export const fetchData = async (movieId) => {
   try {
-    // block the code for 0.5s to 5s, so as not to block the API
-    const delay = Math.floor(Math.random() * 5000) + 500;
+    const delay = Math.floor(Math.random() * 5000) + 2000;
     await new Promise((resolve) => setTimeout(resolve, delay));
-
     const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${tmdbApiKey}&language=en-US`);
     const data = await response.json();
     return data;
@@ -26,8 +27,7 @@ export const uploadImageToImgBB = async (imageBuffer) => {
     formData.append('image', imageBuffer.toString('base64'));
     formData.append('name', 'img');
 
-    const response = await fetch(
-      `https://api.imgbb.com/1/upload?key=${imgbbApiKey}`,
+    const response = await fetch(`https://api.imgbb.com/1/upload?key=${imgbbApiKey}`,
       {
         method: 'POST',
         body: formData,
@@ -35,20 +35,22 @@ export const uploadImageToImgBB = async (imageBuffer) => {
     );
 
     const data = await response.json();
+
     if (data && data.data && data.data.url) {
       const parts = data.data.url.split('/');
+
       if (parts.length >= 5) {
         return parts[3];
       } else {
-        console.error('URL de imgBB incompleta:', data.data.url);
         return null;
       }
+
     } else {
-      console.error('Respuesta de imgBB inválida:', data);
+      console.error('INVALID IMGBB RESPONSE:', data);
       return null;
     }
   } catch (error) {
-    console.error('Error al subir la imagen a imgBB:', error);
+    console.error('Error when IMGBB image upload:', error);
     return null;
   }
 };
@@ -57,33 +59,29 @@ export const processAndUploadImage = async (imageUrl) => {
   try {
     const response = await fetch(imageUrl);
     const imageBuffer = await response.arrayBuffer();
-
     try {
       const processedImage = await sharp(imageBuffer).webp({ quality: 80 }).toBuffer();
       return await uploadImageToImgBB(processedImage);
     } catch (sharpError) {
-      console.error('Error al procesar la imagen con sharp:', sharpError);
+      console.error('Error when processing the image with Sharp:', sharpError);
       return null;
     }
   } catch (error) {
-    console.error('Error al obtener la imagen:', error);
+    console.error('Error obtaining the image:', error);
     return null;
   }
 };
 
-export const saveMovieToDatabase = async (movie) => {
-  const connection = await pool.getConnection(); 
+export const saveMovieToDatabase = async (movie, connection) => {
+  
   const movieId = uuidv4();
-
   let posterId = null;
   let backdropId = null;
-
+  console.log("movie start save img");
+  
   try {
     if (movie.poster_path !== undefined && movie.poster_path !== null) {
-      setTimeout(async () => {
-        posterId = await processAndUploadImage(`https://image.tmdb.org/t/p/w500/${movie.poster_path}`);
-      }, Math.floor(Math.random() * 1000) + 100);
-      
+      posterId = await processAndUploadImage(`https://image.tmdb.org/t/p/w500/${movie.poster_path}`);
     }
   } catch (error) {
     console.error('Error when processing poster:', error);
@@ -91,62 +89,57 @@ export const saveMovieToDatabase = async (movie) => {
 
   try {
     if (movie.backdrop_path !== undefined && movie.backdrop_path !== null) {
-      setTimeout(async() => {
-        backdropId = await processAndUploadImage(`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`);
-      }, Math.floor(Math.random() * 5000) + 100);
-      
+      backdropId = await processAndUploadImage(`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`);
     }
   } catch (error) {
     console.error('Backdrop process error:', error);
   }
 
-
-  await connection.execute(
-    'INSERT IGNORE INTO movies (id, title, original_title, overview, original_language, score, release_date, budget, revenue, runtime, status, tagline, poster_id, backdrop_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())',
-    [
-      movieId,
-      movie.title,
-      movie.original_title,
-      movie.overview,
-      movie.original_language,
-      movie.vote_average,
-      movie.release_date,
-      movie.budget,
-      movie.revenue,
-      movie.runtime,
-      'enabled',
-      movie.tagline,
-      posterId,
-      backdropId,
-    ]
-  );
-
-  return movieId;
-};
-
-export const saveGenreToDatabase = async (name) => {
-  const connection = await pool.getConnection(); 
-  const genreId = uuidv4();
   try {
+    console.log("movie start save data");
 
-    await connection.execute('INSERT IGNORE INTO genres (id, name, created_at, updated_at) VALUES (?, ?, NOW(), NOW())', [genreId, name]);
-
-    return genreId;
+    const [result] = await connection.execute(
+      'INSERT INTO movies (id, title, original_title, overview, original_language, score, release_date, budget, revenue, runtime, status, tagline, poster_id, backdrop_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())',
+      [movieId, movie.title, movie.original_title, movie.overview, movie.original_language, movie.vote_average, movie.release_date || null, movie.budget, movie.revenue, movie.runtime, 'enabled', movie.tagline, posterId, backdropId]
+    );
+  
+    console.log('Resultado de la inserción:', result);
+  
+    if (result.affectedRows > 0) {
+      console.log(`Movie "${movie.title}" Saved correctly with ID: ${movieId}`);
+      return movieId;
+    } else {
+      console.log(`La película "${movie.title}" ya existía o no se pudo guardar.`);
+    }
     
   } catch (error) {
-    console.error('Error by saving gender:', error);
-    throw error; 
-  }
+    console.error('Movie save error:', error);
 
-  
+  }
+    
+
 };
 
-export const saveMovieGenreRelation = async (movieId, genreId) => {
-  const connection = await pool.getConnection(); 
+export const saveGenreToDatabase = async (name, connection) => {
+  const genreId = uuidv4();
+  try {
+    await connection.execute('INSERT IGNORE INTO genres (id, name, created_at, updated_at) VALUES (?, ?, NOW(), NOW())', [genreId, name]);
+    return genreId;
+  } catch (error) {
+    console.error('Error by saving gender:', error);
+    throw error;
+  }
+};
+
+export const saveMovieGenreRelation = async (movieId, genreId, connection) => {
+  console.log('saveMovieGenreRelation', movieId, genreId);
+
+  console.log('connection');
+  
   try {
     await connection.execute('INSERT IGNORE INTO movie_genres (movie_id, genre_id) VALUES (?, ?)', [movieId, genreId]);
   } catch (error) {
     console.error('Error by saving the film-gender relationship:', error);
-    throw error; 
+    throw error;
   }
 };
